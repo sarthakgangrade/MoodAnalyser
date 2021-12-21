68c90496d5fdb62a8c99ddb2c5f4b7b9ae47df26
“sarthak <sarthakgangrade0@gmail.com>
2021-12-22 02:16:55 +0530

    UC1_RefactorUsingConstructor

--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace MoodAnalyzer
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to mood analyzer problem");
             MoodAnalyser moodAnalyse = new MoodAnalyser();
-            string message = "I am in any Mood";
+            string message = "sad";
 
             string result = moodAnalyse.AnalyseMood(message);
             Console.WriteLine(result);
