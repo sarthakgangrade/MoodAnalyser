c60565b84c18793849eea18d1791cdf415284a5f
“sarthak <sarthakgangrade0@gmail.com>
2021-12-22 01:47:36 +0530

    RespondMoodHappySad

---
+++ MoodAnalyzer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to mood analyzer problem");
+            MoodAnalyser moodAnalyse = new MoodAnalyser();
+            string message = "I am in any Mood";
+
+            string result = moodAnalyse.AnalyseMood(message);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
